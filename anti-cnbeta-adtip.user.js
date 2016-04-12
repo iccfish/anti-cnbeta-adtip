66fc72fe4d1597db304c621dc6a208df61153ebb
木鱼(iFish) <iccfish@qq.com>
2016-04-12 17:47:52 +0800

    更新至最新版本

--- anti-cnbeta-adtip.user.js
+++ anti-cnbeta-adtip.user.js
@@ -1,20 +1,24 @@
 // ==UserScript==
 // @name         CnBeta辅助工具
 // @namespace    http://www.fishlee.net/
-// @version      1.0
+// @version      1.1
 // @description  CnBeta上的辅助性工具，用于去除广告等
 // @author       iFish(木鱼)
 // @match        http://www.cnbeta.com/*
 // @grant        unsafeWindow
-// @run-at       document-body
+// @run-at       document-start
 // ==/UserScript==
-(function(window, unsafeWindow, $) {
+(function() {
     'use strict';
-    var oe = unsafeWindow.eval;
-    unsafeWindow.eval = function(code) {
+    var oe = this.eval;
+    var uw = this;
+    uw.eval = function(code) {
         if (code && code.indexOf("J_mask_close") !== -1) {
             return;
         }
         oe.apply(this, Array.prototype.slice.call(arguments));
     };
-})(window, unsafeWindow, unsafeWindow.jQuery);
+    uw.document.addEventListener("DOMContentLoaded", function() {
+        (checkBlock || {}).on = function() {};
+    });
+}).apply(unsafeWindow);
